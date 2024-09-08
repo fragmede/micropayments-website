c419e1f53d897cfa8cec2bea8cce10b9af5c6375
fragmede <samson_gh@onepatchdown.net>
2024-09-08 00:15:10 -0700

    hand edit step to be toggle.

--- src/Step.js
+++ src/Step.js
@@ -1,17 +1,37 @@
 import React from 'react';
 
 class Step extends React.Component {
-  render() {
-    const { title, children } = this.props;
-    const onNext = this.props.onNext || (() => {});
-    return (
-      <div>
-        <h2>{title}</h2>
-        {children}
-        <button onClick={onNext}>Next</button>
-      </div>
-    );
-  }
+    const [isVisible, setIsVisible] = useState(visible);
+
+    useEffect(() => {
+        setIsVisible(visible);
+    }, [visible]);
+
+    const toggleVisibility = () => {
+        setIsVisible(!isVisible);
+    };
+    render() {
+        const { title, children } = this.props;
+        const onNext = this.props.onNext || (() => {});
+        return (
+            <div>
+                <div onClick={toggleVisibility} style={{ cursor: 'pointer', display: 'flex', alignItems: 'center' }}>
+                    <span style={{ transform: isVisible ? 'rotate(90deg)' : 'rotate(0deg)', transition: 'transform 0.2s' }}>
+                        ▶
+                    </span>
+                    <span style={{ marginLeft: '5px' }}>{title}</span>
+                </div>
+                {isVisible && (
+                    <div>
+                        <h2>{title}</h2>
+                        {children}
+                        <button onClick={onNext}>Next</button>
+                    </div>
+                )
+                }
+            </div>
+        );
+    }
 }
 
 export default Step;

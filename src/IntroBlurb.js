ebd0d962383b92e88590768ac57b53600b8cf40b
fragmede <samson_gh@onepatchdown.net>
2024-10-13 07:32:37 -0700

    feat: Update IntroBlurb component with new heading and description

--- src/IntroBlurb.js
+++ src/IntroBlurb.js
@@ -3,7 +3,8 @@ import React from 'react';
 const IntroBlurb = () => {
   return (
     <div>
-      <p>Welcome to Micropayments.fyi! This platform allows you to easily manage and make micropayments using Solana. Follow the steps below to get started.</p>
+      <h3>Welcome to Micropayments.fyi!</h3>
+      <p> This page will walk you through the steps to make a $0.10 USD micropayment. Follow the steps below to get started.</p>
     </div>
   );
 };
